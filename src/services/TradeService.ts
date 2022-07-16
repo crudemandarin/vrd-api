f2f289c1631fffdbefdd88928667d163bf15736c
Nykolas Farhangi <nykolasfarhangi@gmail.com>
2022-07-16 16:06:22 -0500

    implemented create many trades and update trade

--- src/services/TradeService.ts
+++ src/services/TradeService.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-import { TradeModel } from "../models/trade.model";
 
+import { TradeModel } from "../models/trade.model";
 import logger from "../utils/logger/logger";
 
 const prisma = new PrismaClient();
@@ -19,7 +19,9 @@ class TradeService {
 	}
 
 	static async createTrade(trade: TradeModel) {
-		logger.info("TradeService.createTrade invoked!");
+		logger.info(
+			`TradeService.createTrade invoked! trade = ${JSON.stringify(trade)}`
+		);
 
 		const dates = {
 			date: new Date(trade.date),
@@ -38,15 +40,60 @@ class TradeService {
 		return result;
 	}
 
-	static async updateTrade() {
-		logger.info("TradeService.updateTrade invoked!");
-	}
+	static async createTrades(trades: TradeModel[]) {
+		logger.info(
+			`TradeService.createTrades invoked! trades = ${JSON.stringify(trades)}`
+		);
 
-	static async deactivateTrade() {
-		logger.info("TradeService.deactivateTrade invoked!");
-		const result = await this.updateTrade();
+		const format = trades.map((trade) => {
+			const dates = {
+				date: new Date(trade.date),
+				deliveryStart: new Date(trade.deliveryStart),
+				deliveryEnd: new Date(trade.deliveryEnd),
+				expiration: new Date(trade.expiration),
+			};
+			return { ...trade, ...dates };
+		});
+
+		const result = await prisma.trade.createMany({
+			data: format,
+		});
+
 		return result;
 	}
+
+	static async updateTrade(id: string, updates: TradeModel) {
+		logger.info(
+			`TradeService.updateTrade invoked! id = ${id}, updates = ${JSON.stringify(
+				updates
+			)}`
+		);
+
+		const dates = {
+			date: new Date(updates.date),
+			deliveryStart: new Date(updates.deliveryStart),
+			deliveryEnd: new Date(updates.deliveryEnd),
+			expiration: new Date(updates.expiration),
+		};
+		const format = { ...updates, ...dates };
+
+		const result = await prisma.trade.update({
+			where: {
+				id,
+			},
+			data: {
+				...format,
+			},
+		});
+
+		return result;
+	}
+
+	// static async deactivateTrade(id: string) {
+	// 	logger.info("TradeService.deactivateTrade invoked!");
+	// 	const result = await this.updateTrade(id, {});
+	// 	return result;
+	// }
 }
 
 export default TradeService;
